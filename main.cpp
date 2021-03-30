f3c70da92da6d41c612d9ac9b085abf7fe812ac0
TannerYork <tannerwyork@gmail.com>
2021-03-30 11:01:04 -0500

    Add basics for creating windows for the animation

--- main.cpp
+++ main.cpp
@@ -4,13 +4,40 @@ using namespace std;
 
 int main() {
 
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
     if(SDL_Init(SDL_INIT_VIDEO) > 0) {
         cout << "SDL init failed." << endl;
         return 1;
     }
     
-    cout << "SDL init succeeded." << endl;
+    SDL_Window *window = SDL_CreateWindow("ParticleFireExplosion", 
+         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+         SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+    if(window == NULL) {
+        cout << "Could not create window: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return 2;
+    }
+
+    bool quit = false;
+    SDL_Event event;
+
+    while(!quit) {
+        // Update particals
+        // Draw particals
+        // Check for messages/events
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
